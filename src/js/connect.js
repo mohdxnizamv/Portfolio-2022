6beb84c0e809a0b8b596196274dce3c7efc95b97
Mohd Nizamuddin Mad Yusof <58153279+mohdxnizamv@users.noreply.github.com>
2022-11-16 22:38:19 +0800

    first commit

--- src/js/connect.js
+++ src/js/connect.js
@@ -1,32 +1,40 @@
-//----------------------------------------------------------------------------------------------
-// Account Change Metamask
-window.ethereum.on('accountsChanged', async function (accounts) {
-  // Time to reload your interface with accounts[0]!
-  account = accounts[0];
-
-  if (Accounts != null) {
-    AccountBalance();
-    document.getElementById('wallet-address').textContent = account;
-  }
-});
-
-//----------------------------------------------------------------------------------------------
-// Disconnect Metamask
-window.ethereum.on('disconnect', function (networkId) {
-  // Time to reload your interface with the new networkId
-});
-
+//CONNECT AUTOMETAMASK
 document.getElementById('connect').onclick = () => {
-  // CONNECT METAMASK
-  (async () => {
-    if (window.ethereum) {
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      window.web3 = new Web3(window.ethereum);
-      var accounts = await web3.eth.getAccounts();
-      account = accounts[0];
-      Accounts = account;
-      document.getElementById('wallet-address').textContent = account;
-      AccountBalance();
-    }
-  })();
+    (async () => {
+        if (window.ethereum) {
+            await window.ethereum.request({
+                method: 'eth_requestAccounts',
+            });
+            window.web3 = new Web3(window.ethereum);
+            var accounts = await web3.eth.getAccounts();
+            account = accounts[0];
+            Accounts = account;
+            document.getElementById('wallet-address').textContent = account;
+            document.getElementById('connect').className = 'butang';
+            document.getElementById('connect').innerHTML = 'CONNECTED';
+
+            // getTokenPrice();
+        }
+
+        //----------------------------------------------------------------------------------------------
+        //ENABLED ALL BUTTON
+        function getElementsByIds(ids) {
+            var idList = ids.split(' ');
+            var results = [],
+                btn;
+            for (var i = 0; i < idList.length; i++) {
+                btn = document.getElementById(idList[i]);
+                if (btn) {
+                    results.push(btn);
+                    btn.disabled = false;
+                }
+            }
+            return results;
+        }
+        someFunc(getElementsByIds(''));
+
+        function someFunc(ids) {}
+
+        //----------------------------------------------------------------------------------------------
+    })();
 };
